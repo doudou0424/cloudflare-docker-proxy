2ad282c1800b8849873e2cb7a8bbb8a4f3817cc2
doudou <1142589809@qq.com>
2024-06-19 11:29:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.yanzhiwei.xyz": dockerHub,
+  "quay.yanzhiwei.xyz": "https://quay.io",
+  "gcr.yanzhiwei.xyz": "https://gcr.io",
+  "k8s-gcr.yanzhiwei.xyz": "https://k8s.gcr.io",
+  "k8s.yanzhiwei.xyz": "https://registry.k8s.io",
+  "ghcr.yanzhiwei.xyz": "https://ghcr.io",
+  "cloudsmith.yanzhiwei.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.yanzhiwei.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
